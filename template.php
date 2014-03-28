43eb0189e8daf008c59a2839118e95a04477fab7
heliogabal <rhalbmann@gmail.com>
2014-03-28 06:24:53 +0100

    almost finished conventional

--- template.php
+++ template.php
@@ -20,3 +20,37 @@ function inkshop_theme_form_alter(&$form, &$form_state, $form_id) {
     break;
   }
 }
+
+function inkshop_theme_entity_property_info_alter(&$info){
+  $info['node']['properties']['sort_price'] = array(
+     'type' => 'decimal',
+     'label' => 'Sort price',
+     'description' => 'The price to sort this product by.',
+     'getter callback' => '_inkshop_theme_get_sort_price',
+  );
+}
+/*
+* getter callback for sort price property
+*
+* return the lowest product price associated with this display
+*
+*/
+function _inkshop_theme_get_sort_price($data, array $options, $name, $type, $info){
+  if($name == 'sort_price'){
+    $wrapper = entity_metadata_wrapper('node', $data);
+    //check if this is a product display
+    if(array_key_exists('field_product', $wrapper->getPropertyInfo())){
+      $prices = array();
+      //loop trough referenced products
+      foreach ($wrapper->field_product->getIterator() as $delta => $term_wrapper) {
+        //get value of commerce_price
+        $price_field = $term_wrapper->commerce_price->value();
+        //transform commerce price to decimal
+        $prices[] = commerce_currency_amount_to_decimal($price_field['amount']);
+      }
+      //return lowest price for this display
+      return min($prices);
+    }
+  }
+  return NULL;
+}
